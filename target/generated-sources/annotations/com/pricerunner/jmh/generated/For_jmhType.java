fbf5086e678f731ba3c0171213868afc3e5ada3c
Johan Wiström <johan.wistroem@gmail.com>
2019-02-14 11:03:25 +0100

    removed untracked files

--- target/generated-sources/annotations/com/pricerunner/jmh/generated/For_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.pricerunner.jmh.generated;
-public class For_jmhType extends For_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/com/pricerunner/jmh/generated/LambdaOrMethodReference_jmhType.java
+++
@@ -1,4 +0,0 @@
-package com.pricerunner.jmh.generated;
-public class LambdaOrMethodReference_jmhType extends LambdaOrMethodReference_jmhType_B3 {
-}
-
